2f89f42a6289ae8a4490862b4031228add182fce
Jan Unverdorben <honza.unverdorben@gmail.com>
2025-04-14 12:37:15 +0200

    Update-1

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -10,9 +10,20 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
 
-
+    return data
 def main():
+    data = read_data("numbers.csv")
+    print(data)
     pass
 
 
